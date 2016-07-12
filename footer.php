9c13f8ca9984db8dac3f7b60c8d703d33c497e8b
Jarod Means <jsmeans2@gmail.com>
2016-07-12 18:46:15 -0500

    first commit, setting up Sass, Susy and Wordpress files

---
+++ footer.php
@@ -0,0 +1,21 @@
+		
+
+		<footer>
+		<img src="<?php bloginfo('template_directory'); ?>/images/footerimg.jpg">
+
+			<div  class="container">
+
+				<div class="studio42">
+		        		<a href="http://www.studio-42.com">Site Design and Development by Studio 42</a>
+		        </div>
+
+			</div>
+
+		</footer>
+
+		<?php wp_footer(); ?>
+		
+   
+	</body>
+</html>
+
---
+++ front-page.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+
+
+
+<section class="date">
+  <h1>11.12.16</h1><br/>
+  <h1>15h30</h1>
+  
+</section>
+
+
+<?php get_template_part('content', 'rsvp'); ?>
+
+
+<section class="homenav">
+  <ul>
+    <li>
+      <h1>How We Met</h1>
+    </li>
+    <li>
+      <h1>Accommodation</h1>
+    </li>
+    <li>
+      <h1>Fashion and Themes</h1>
+    </li>
+    <li>
+      <h1>Gift Registry</h1>
+    </li>
+  </ul>
+  
+</section>
+
+<?php get_template_part('content', 'portfolio'); ?>
+
+<?php get_footer(); ?>
