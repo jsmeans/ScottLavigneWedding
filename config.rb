49be51c7164879835dc7deef84db1efb1439a3f3
Jarod Means <jsmeans2@gmail.com>
2016-08-16 11:46:19 -0500

    compressing sass files, small design changes

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ css_dir = "." #targets our default style.css file at the root level of our theme
 sass_dir = "sass" #targets our sass directory
 images_dir = "images" #targets our pre existing image directory
 javascripts_dir = "js" #targets our JavaScript directory
-output_style = :nested
+output_style = :compressed
 relative_assets = true
 
 require 'susy'
